565c342be7807580881fa3afead4240ee1a3de1f
Ethan Klukkert <ethan7klukkert@gmail.com>
2022-07-13 16:38:56 -0400

    styling and scoring system

--- index.js
+++ index.js
@@ -2,7 +2,20 @@
 const geoEl = document.getElementById("geography")
 const historyEl = document.getElementById("history")
 const frenchEl = document.getElementById("french")
+const resetBtn = document.getElementById("reset-score-btn")
 
+function setScore() {
+    const correctEl = document.getElementById("correct")
+    const attemptedEl = document.getElementById("attempted")
+    if (localStorage.getItem("correct") && localStorage.getItem("attempted")) {
+        correctEl.textContent = localStorage.getItem("correct")
+        attemptedEl.textContent = localStorage.getItem("attempted")
+    } 
+    else {
+        correctEl.textContent = "--"
+        attemptedEl.textContent = "--"
+    }
+}
 
 geoEl.addEventListener("click", function() {
     console.log("Geography")
@@ -19,23 +32,69 @@ frenchEl.addEventListener("click", function() {
     sessionStorage.setItem("currentQuiz", "french")
 })
 
+resetBtn.addEventListener("click", function() {
+    localStorage.removeItem("correct")
+    localStorage.removeItem("attempted")
+    setScore()
+})
 
 standardPackage = {
     geography: {
         questionOne: {
             question: "What mountain range runs along South America?",
             answer: "Andes",
-            wrongs: ["Atlas", "Appalacian", "Rockies"],
+            wrongs: ["Atlas", "Appalacian", "Rockies"]
         },
         questionTwo: {
             question: "What body of water borders south west of Russia?",
             answer: "Black Sea",
             wrongs: ["Baltic Sea", "South China Sea", "Mediterranean Sea"]
+        },
+        questionThree: {
+            question: "What desert is in western USA?",
+            answer: "Mojave",
+            wrongs: ["Sahara", "Gobi", "Arabian"]
+        },
+    },
+    history: {
+        questionOne: {
+            question: "The Holy Roman Empire's territory was mainly modern ____?",
+            answer: "Germany", 
+            wrongs: ["France", "Italy", "Spain"]
+        },
+        questionTwo: {
+            question: "Who was the president of the United States during World War One?",
+            answer: "Woodrow Wilson",
+            wrongs: ["Franklin D. Roosevelt", "George Washington", "Dwight D. Eisenhower"]
+        },
+        questionThree: {
+            question: "Which empire was NOT dismantled after World War One?",
+            answer: "French",
+            wrongs: ["Ottoman", "Russian", "German"]
+        }
+    },
+    french: {
+        questionOne: {
+            question: "\"Inondation\" means what in English?",
+            answer: "Flood", 
+            wrongs: ["Swarm", "Waterfall", "Wave"]
+        },
+        questionTwo: {
+            question: "",
+            answer: "",
+            wrongs: []
+        },
+        questionThree: {
+            question: "How many letters are silent in any French word?",
+            answer: "Depends",
+            wrongs: ["None of them", "Who knows?", "All of them"]
         }
     }
 }
 
 console.log()
+
+setScore()
 
 standard = JSON.stringify(standardPackage)
 localStorage.setItem("standardPackage", standard)
--- quizMenu.js
+++ quizMenu.js
@@ -1,31 +1,32 @@
 
-const standardQuizzes = ["geography", "history", "french", "movies", "books"]
+const standardQuizzes = ["geography", "history", "french"]
 const quizEl = document.getElementById("quiz-el")
 let createdQuizzes = ["topology"]  // make from localStorage automatically
 
 let standardPackage = localStorage.getItem("standardPackage")
 standardPackage = JSON.parse(standardPackage)
+console.log(standardPackage)
 
 let selectedQuiz = sessionStorage.getItem("currentQuiz")
 console.log(selectedQuiz)
-console.log(standardPackage)
+let quizTitle = document.getElementById("quiz-title")
+quizTitle.textContent = selectedQuiz.charAt(0).toUpperCase() + selectedQuiz.slice(1)
 
 // prepares quiz string for innerHTML. makes and reserves slots for 4 option questions using random placement
 questions = Object.values(standardPackage[selectedQuiz])
+quizLength = questions.length
 htmlQuizData = ""
-for (let i = 0; i < questions.length; i++) {
+for (let i = 0; i < quizLength; i++) {
 
     slots = [1, 2, 3, 4]
     orderOfQuestions = []
     while (slots.length != 0) {
         const randomIndex = Math.floor(Math.random() * slots.length)
         const randomSlot = slots[randomIndex]
-        console.log(randomSlot)
         orderOfQuestions.push(randomSlot)
         slots.splice(randomIndex, 1)
     }
 
-    console.log("ORDER:" + orderOfQuestions)
     const options = [questions[i].answer, questions[i].wrongs[0], questions[i].wrongs[1], questions[i].wrongs[2]]
 
     for (let k = 0; k < orderOfQuestions.length; k++) {
@@ -48,27 +49,26 @@ for (let i = 0; i < questions.length; i++) {
 
     htmlQuizData += `
     <div class="question-div-el">
-        
         <li class="question-el"><p class="question-text">${questions[i].question}</p>
             <ul class="answers-ul-el">
                 <li class="answer-el"> 
                     <label class="answer-label">
-                        <input type="radio" name="${i}" id="${i}0"> <p>${slotOne}</p>
+                        <input type="radio" name="${i}" id="${i}0"> <p id="${i}00">${slotOne}</p>
                     </label>
                 </li>
                 <li class="answer-el">
                     <label class="answer-label">
-                        <input type="radio" name="${i}"> <p>${slotTwo}</p>
+                        <input type="radio" name="${i}" id="${i}1"> <p id="${i}10">${slotTwo}</p>
                     </label>
                 </li>
                 <li class="answer-el"> 
                     <label class="answer-label">
-                        <input type="radio" name="${i}"> <p>${slotThree}</p>
+                        <input type="radio" name="${i}" id="${i}2"> <p id="${i}20">${slotThree}</p>
                     </label>
                 </li>
                 <li class="answer-el">
                     <label class="answer-label">
-                        <input type="radio" name="${i}"> <p>${slotFour}</p>
+                        <input type="radio" name="${i}" id="${i}3"> <p id="${i}30">${slotFour}</p>
                     </label>
                 </li>
             </ul>
@@ -80,26 +80,60 @@ for (let i = 0; i < questions.length; i++) {
 // render quiz
 quizEl.innerHTML = htmlQuizData
 
-function checkAnswer() {
-    const ele = document.querySelector('#\\30 \\30 ')  // I hate unicode. 
-    console.log(ele.textContent)  // RETURN WORK HERE, how the heck am I gonna gather the answers properly?
-    // GIT commit? made selecting for questions
-    const allChecked = document.querySelectorAll('.answer-label.checked')
-    console.log(allChecked)
+function checkAnswers() {
+    // id's work as follows: "02" --> is question one, answer 3's input. "120" --> is question two, answer 3's text
+    let submittedAnswers = {}
+    let numCorrect = 0
+    for (let i = 0; i < quizLength; i++) {
+        for (let j = 0; j < 4; j++) {
+            const ele = document.querySelector(`#\\3${i} \\3${j} `)  // finds which answer is selected
+            if (ele.checked) {
+                answerText = document.querySelector(`#\\3${i} \\3${j} \\30 `)  // finds the text of selected
+                submittedAnswers[i] = `${answerText.textContent}`
+                console.log(answerText.textContent)
+            }
+        }
+    }
+    console.log(submittedAnswers)
+
+    for (let k = 0; k < quizLength; k++) {
+        if (questions[k].answer === submittedAnswers[k]) {
+            numCorrect++
+        }
+    }
+    console.log(`Your grade: ${numCorrect}/${quizLength}`)
+    if (localStorage.getItem("correct") && localStorage.getItem("attempted")) {
+        let correctPreviousScore = localStorage.getItem("correct")
+        let attemptedPreviousScore = localStorage.getItem("attempted")
+
+        let correctScore = numCorrect + Number(correctPreviousScore)
+        let attemptedScore = quizLength + Number(attemptedPreviousScore)
+
+        localStorage.setItem("correct", correctScore.toString())
+        localStorage.setItem("attempted", attemptedScore.toString())
+    } 
+    else {
+        localStorage.setItem("correct", numCorrect.toString())
+        localStorage.setItem("attempted", quizLength)
+    }
+
+    const submitDiv = document.getElementById("submit-div")
+    submitDiv.innerHTML = `
+    <p>You Scored: ${numCorrect} out of ${quizLength} (${Math.round((numCorrect/quizLength) * 100)}%)</p>
+    <button id="return-btn" onclick="location.href='index.html'">Return To Home</button>
+    `
 }
 
 // will be needed for "questions" variable
 function findQuiz(word) {
     for (let i = 0; i < standardQuizzes.length; i++) {
         if (word === standardQuizzes[i]) {
-            // startQuiz(word)
             console.log("found quiz in standardQuizzes")
             return
         }
     }
     for (let i = 0; i < createdQuizzes.length; i++) {
         if (word === createdQuizzes[i]) {
-            // startQuiz(word)
             console.log("found quiz in createdQuizzes")
             return
         }
@@ -110,8 +144,4 @@ function findQuiz(word) {
 
 function submitAndCheckAnswers() {
     // given length of the quiz, loop by querySelector using (XY) as ID's, where x is question, y is answer.
-}
-
-function writeThing() {
-    console.log("clicked, gather info")
 }
